9dcf4853aa4718ba22e2d6597ee82bca3db83d15
PG-12 <79004509+PG-12@users.noreply.github.com>
2021-02-16 01:10:58 +0800

    phish

--- phish.sh
+++ phish.sh
@@ -69,6 +69,7 @@ kill_pid() {
 ## Banner
 banner() {
 	cat <<- EOF
+
         ${ORANGE}
 	${ORANGE{ #######  #######   //#  #######
         ${ORANGE} #     #  #        //##        #            
@@ -85,6 +86,7 @@ banner() {
 ## Small Banner
 banner_small() {
 	cat <<- EOF
+
         ${ORANGE}
 	${ORANGE{ #######  #######   //#  #######
         ${ORANGE} #     #  #        //##        #            
